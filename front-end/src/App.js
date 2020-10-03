9ce29092efc04cae6e105389a6552832864d93dd
Kieran Milligan <kieran.milligan@gmail.com>
2020-10-03 18:04:09 -0500

    finished login and registration

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,8 +5,13 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const userSignin = useSelector(state=>state.userSignin);
+  const {userInfo} = userSignin;
 
   const openMenu = () => {
     document.querySelector(".sidebar").classList.add("open");
@@ -26,7 +31,10 @@ function App() {
           </div>
           <div className="header-links">
             <a href="cart.html">Cart</a>
-            <Link to="/signin">Sign In</Link>
+            {
+              userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+              <Link to="/signin">Sign In</Link>
+            }
           </div>
         </header>
 
@@ -47,6 +55,7 @@ function App() {
         <main className="main">
           <div className="content"> {/* Change Routes to display custom landing page, etc. */}
             <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/cart/:id?" component={CartScreen} />
             <Route path="/" exact={true} component={HomeScreen} />
